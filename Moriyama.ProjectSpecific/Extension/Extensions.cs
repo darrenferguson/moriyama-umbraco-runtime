38996e37db37d09e58c1922638e028731becf285
unknown <darren.ferguson@Moriyama4.moriyama.local>
2015-01-02 16:43:45 +0000

    Basic views complete

---
+++ Moriyama.ProjectSpecific/Extension/Extensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Moriyama.Blog.Project.Extension
+{
+    public static class Extensions
+    {
+        public static string ToOrdinal(this int number)
+        {
+            switch (number % 100)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return number.ToString() + "th";
+            }
+
+            switch (number % 10)
+            {
+                case 1:
+                    return number.ToString() + "st";
+                case 2:
+                    return number.ToString() + "nd";
+                case 3:
+                    return number.ToString() + "rd";
+                default:
+                    return number.ToString() + "th";
+            }
+        }
+    }
+}
